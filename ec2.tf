db0c382377f80c38dd732d58f19925a5847927f4
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-05-05 18:06:58 -0500

    added tfvars

--- ec2.tf
+++ ec2.tf
@@ -3,20 +3,3 @@ resource "aws_instance" "instance" {
   instance_type     = var.instance_type
   key_name          = aws_key_pair.mykey.key_name
   vpc_security_group_ids = [aws_security_group.ssh.id,aws_security_group.web.id]
-  
-
-  
-  provisioner "remote-exec" {
-    connection {
-      host          = self.public_ip
-      type          = "ssh"
-      user          = "centos"
-      private_key   = file(var.PATH_TO_PRIVATE_KEY)
-      }
-      inline = [
-        "sudo yum install httpd -y",
-        "sudo systemctl start httpd && sudo systemctl enable httpd",
-        ]
-      } 
-
-}
